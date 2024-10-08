0dbbc6b369ce9a430b0695c84bd833e41ea3b9e6
munaimun <munaimunz@gmail.com>
2024-10-08 18:43:18 +0600

    Create Node in Linked List

--- oop_1.py
+++ oop_1.py
@@ -1,15 +1,22 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name = name;
-        self.marks = marks;
-                
-    #Defining the method to get avg marks
-    def avg_mark(self):
-        sum = 0;
-        for val in self.marks:
-            sum += val;
-        print(f"Hello {self.name}, your average mark is {sum/3}")
-            
-s1 = Student("Fahad", [99, 98, 97])
+#You must make a program that read a float-point number and print a message saying in which of following intervals the number belongs: [0,25] (25,50], (50,75], (75,100]. If the read number is less than zero or greather than 100, the program must print the message “Fora de intervalo” that means "Out of Interval".
+#The symbol '(' represents greather than. For example:
+#[0,25] indicates numbers between 0 and 25.0000, including both.
+#(25,50] indicates numbers greather than 25 (25.00001) up to 50.0000000.
 
-s1.avg_mark()
+
+def check_interval(A):
+    if 0 <= A <= 25:
+        print("[0,25]")
+    elif 25 < A <=50:
+        print("(25,50]")
+    elif 50 < A <= 75:
+        print("(50,75")
+    elif 75 < A <= 100:
+        print("(75,100]")
+    else:
+        print("Fora de intervalo")
+
+
+a = float(input())
+
+check_interval(a)
